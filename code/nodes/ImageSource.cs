80126f680062a969a64b688bb44bb1df72b9f0a9
SimonStorlSchulke <simonstoschu@googlemail.com>
2020-08-12 23:55:56 +0200

    File Selection Popup

--- code/nodes/ImageSource.cs
+++ code/nodes/ImageSource.cs
@@ -7,19 +7,47 @@ public class ImageSource : NodeBase {
 
     Image img = new Image();
     ImageTexture tex = new ImageTexture();
+    FileDialog fileDialog;
 
     public override void _Ready() {
         base._Ready();
+        fileDialog = GetNode<FileDialog>("FileDialog");
         pathInput = GetNode<TextEdit>("VBoxContainer/HBoxContainer/Path");
         viewer = GetNode<TextureRect>("VBoxContainer/ImageViewer");
+
+        fileDialog.Filters = new string[]{
+            "*.bmp ; BMP",
+            "*.jpg ; JPG",
+            "*.jpeg ; JPEG",
+            "*.png ; PNG",
+            "*.svg ; SVG",
+            "*.tga ; TGA",
+            "*.webp ; WEBP",
+            };
+        fileDialog.PopupCentered();
     }
 
-    public void OnPathUpdate() {
+    void setImage(string path) {
         //Draw Image -> extract Method later
-        img.Load(pathInput.Text);
+        img.Load(path);
         if (img == null)
             return;
         tex.CreateFromImage(img);
         viewer.Texture = tex;
     }
+
+    void OnOpenFileDialog() {
+       fileDialog.PopupCentered();
+    }
+
+    void OnPathUpdate() {
+        setImage(pathInput.Text);
+    }
+
+    void OnFileSelected(string path) {
+        pathInput.Text = path;
+        setImage(path);
+    }
+
+
 }
