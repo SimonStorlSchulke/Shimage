d03b4d83935c22b7e79c845d0395ff5cd8f105e2
SimonStorlSchulke <simonstoschu@googlemail.com>
2021-05-11 19:49:12 +0200

    Shaderfilter Props Structure

--- GodotProject/code/imaging/Shaderer.cs
+++ GodotProject/code/imaging/Shaderer.cs
@@ -4,22 +4,55 @@ using System;
 public class Shaderer : Sprite {
 
     public struct ShaderFilter {
+        public string Name;
         public string Code;
         public string UniformsCode;
+        public Prop[] props;
 
         //TODO save uniforms as individual variables and generate code from them.
-        public ShaderFilter(string _uniforms, string _code) {
-            Code = _code;
-            UniformsCode = _uniforms;
+        public ShaderFilter(string _name, Prop[] _props, string _code) {
+            this.Name = _name;
+            this.Code = _code;
+            this.props = _props;
+
+            this.UniformsCode = "";
+            foreach (var cProp in props) {
+                this.UniformsCode += cProp.GetUniformCode() + "\n";
+            }
+        }
+
+        void GenerateUniformsCode() {
+
         }
     }
 
     ShaderFilter shaderVignette = new ShaderFilter(
-        "uniform float vignettePower = 2.0;", @"
+        "Vignette", new Prop[] {
+            new PropFloat("vignettePower", 2.0f)
+        },
+        @"
         //ShaderVignette
         float d = distance(UV, vec2(0.5, 0.5));
         d = 1.0 - pow(d, vignettePower);
         COLOR *= vec4(d,d,d,1);
+    ");
+
+    ShaderFilter shaderExposure = new ShaderFilter(
+        "Exposure",
+         new Prop[] {
+            new PropFloat("exposure", 1.0f)
+        },
+        @"
+        COLOR *= vec4(exposure,exposure,exposure,1);
+    ");
+
+    ShaderFilter shaderMultiplyColor = new ShaderFilter(
+        "Overlay Color",
+        new Prop[] {
+            new PropRGB("multiplycolor", new Color(1,1,1))
+        },
+        @"
+        COLOR *= vec4(multiplycolor.rgb, 1);
     ");
 
     public string GenerateShader(ShaderFilter[] shaders) {
@@ -45,10 +78,16 @@ public class Shaderer : Sprite {
     public override void _Ready() {
         ShaderMaterial m = new ShaderMaterial();
         Shader shader = new Shader();
-        shader.Code = GenerateShader(new ShaderFilter[] { shaderVignette });
+        shader.Code = GenerateShader(new ShaderFilter[] { 
+            shaderVignette, 
+            shaderExposure,
+            shaderMultiplyColor,
+            });
         m.Shader = shader;
         Material = m;
         m.SetShaderParam("vignettePower", 0.4f);
+        m.SetShaderParam("exposure", 2f);
+        m.SetShaderParam("multiplycolor", new Vector3(1,0.6f,0.7f));
         GD.Print(Material);
     }
 }
--- GodotProject/code/ui/props.cs
+++ GodotProject/code/ui/props.cs
@@ -1,11 +1,55 @@
 using Godot;
 using System;
 
-public interface IProp {
-    void BuildUI();
+public abstract class Prop {
+    public string Name;
+    public object Value;
+    public abstract void BuildUI();
+    public abstract string GetUniformCode();
 }
 
-public struct PropInt
-{
+public class PropInt : Prop {
 
+    public PropInt(string _name, int _value) {
+        this.Name = _name;
+        this.Value = _value;
+    }
+
+    public override void BuildUI() {
+    }
+
+    public override string GetUniformCode() {
+        return "uniform int " + this.Name + " = " + Value + ";";
+    }
 }
+
+public class PropFloat : Prop {
+
+    public PropFloat(string _name, float _value) {
+        this.Name = _name;
+        this.Value = _value;
+    }
+
+    public override void BuildUI() {
+    }
+
+    public override string GetUniformCode() {
+        return "uniform float " + this.Name + " = " + Value + ";";
+    }
+}
+
+public class PropRGB : Prop {
+
+    public PropRGB(string _name, Color _value) {
+        this.Name = _name;
+        this.Value = _value;
+    }
+
+    public override void BuildUI() {
+    }
+
+    public override string GetUniformCode() {
+        return $"uniform vec3 {this.Name} = vec3({((Color)Value).r}, {((Color)Value).g}, {((Color)Value).b});";    
+        }
+}
+
